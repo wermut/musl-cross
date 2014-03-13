d5b7082b4f09c8b4af4032e1ea3b15e49436680b
Kevin Bortis <kevin@bortis.ch>
2014-03-13 09:25:42 +0100

    Added custom config templates
    Template for:
    - Default armhf
    - Raspberry Pi
    - Default softfp
    
    Added aarch64 as ARCH choice

--- config.sh
+++ config.sh
@@ -3,6 +3,7 @@
 #ARCH=x86_64
 #ARCH=powerpc
 #ARCH=arm
+#ARCH=aarch64
 #ARCH=microblaze
 #ARCH=mips
 #ARCH=mipsel
@@ -10,12 +11,20 @@
 CC_BASE_PREFIX=/opt/cross
 
 # If you use arm, you may need more fine-tuning:
-# arm hardfloat v7
+# A safe choice for hard float on armv7: (Debian default for armhf)
+#ARCH=arm
 #TRIPLE=arm-linux-musleabihf
 #GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
 #GCC_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
 
+# For Raspberry Pi (RPi):
+#ARCH=arm
+#TRIPLE=arm-linux-musleabihf
+#GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv6-a --with-float=hard --with-fpu=vfpv2"
+#GCC_CONFFLAGS="--with-arch=armv6-a --with-float=hard --with-fpu=vfpv2"
+
 # arm softfp
+#ARCH=arm
 #TRIPLE=arm-linux-musleabi
 #GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
 #GCC_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
